6ce23bcb4a2811c9e68de35b94b0fdd16fec084b
saul-manich <saulmamani73@gmail.com>
2017-10-23 14:50:05 -0400

    Veganomania
    Html5 Css3 Js Bootstrap Veganomania

---
+++ Veganom/process.php
@@ -0,0 +1,23 @@
+<?php
+
+    $to = "";
+    $from = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"subject"} = "subject";
+    $fields{"message"} = "message";
+
+    $body = "\n\n";
+     foreach($fields as $a => $b)
+     {   
+         $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]);
+     }
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
